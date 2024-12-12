cb70900eaeaabb129371faba832c77b85ba26df7
Luis Altuve <laltuve@ext.sebbusiness.com>
2024-12-12 15:42:29 -0500

    Deleting the old folder structure

--- Event-Handling/EventObject/app.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector("button");
-
-const buttonClicked = (event) => {
-  console.log(event.target);
-  console.log(event.target.id);
-  console.log(event.target.textContent);
-};
-
-button.addEventListener("click", buttonClicked);
